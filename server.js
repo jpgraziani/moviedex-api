e0561a4f63b33e25458929da27dc50b5876f5f4e
Jonathan Graziani <jpgraziani@gmail.com>
2021-01-03 22:24:10 -0700

    updated boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hi')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use((req, res) => {
+  res.send('hello olivia')
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
+
