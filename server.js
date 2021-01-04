4384735e92cd334167a0abbd571417475b17434c
Jonathan Graziani <jpgraziani@gmail.com>
2021-01-04 09:39:19 -0700

    added helmet

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const cors = require('cors');
 
 const MOVIEDATA = require('./moviedata.json');
@@ -10,6 +11,7 @@ console.log(process.env.API_TOKEN)
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
 app.use(cors())
 
 //put validation here
